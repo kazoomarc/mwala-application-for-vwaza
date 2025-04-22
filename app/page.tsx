635477ba4a093bfa0e1fadf86bf9c99d3495bd32
Joel Mwala <joelmwala7@gmail.com>
2025-04-23 00:32:12 +0200

    add text

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,108 @@
 import Image from "next/image";
+const images = ["img1.jpg", "img2.jpg", "img3.jpg"];
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="bg-[rgb(8,8,8)] min-h-screen font-sans font-[family-name:var(--font-geist-sans)">
+      <div className="mx-3.5 border-1 _border-x-blue-200 border-dashed">
+        <div className="max-w-2xl flex flex-col gap-6">
+          <h1 className="text-white text-3xl font-bold mt-12 ">
+            Hi, I'm Joel Mwala{" "}
+          </h1>
+          <p className="text-[#d5d5d5] text-[15px]">
+            This is my application portfolio for the Graphic Designer position
+            at Vwaza Multimedia Inc. After seeing your post Friday, I
+            immediately felt inspired and locked in to create custom visuals
+            from Saturday through Monday that align with your needs.
+          </p>
+          <p className="text-[#d5d5d5] text-[15px]">
+            What you see before you is a current showcase of my visual design
+            skills tailored specifically for Vwaza as a music streaming
+            platform. I understand that in mainstream media, designs must both
+            deliver clear messages and be visually appealing to capture the
+            already declining attention span of social media users. This
+            approach is key to increasing user engagement with the Vwaza app and
+            driving conversions.
+          </p>
+          <p className="text-[#d5d5d5] text-[15px]">
+            I care deeply about details and quality in every piece I create.
+            These visuals demonstrate my versatile use of typography, colour,
+            and form across print, screen, and web formats.
+          </p>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <h2 className="text-white font-bold text-3xl mt-[18px] border-1 border-[transparent] border-t-[#ffffff14] pt-2">
+            Explorations
+          </h2>
+          <div mt-6>
+            {images.map((img, index) => (
+              <div key={index} style={{ marginBottom: "1rem" }}>
+                <Image
+                  src={`/images/${img}`}
+                  alt={`Image ${index + 1}`}
+                  width={400}
+                  height={300}
+                />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div>
+          <h2 className="text-white font-bold text-3xl mt-[18px] border-1 border-[transparent] border-t-[#ffffff14] pt-2">
+            Closing Remarks(Extras about the works)
+          </h2>
+          <div className=" max-w-2xl flex flex-col gap-6">
+            <p className="text-[#d5d5d5] text-[15px] mt-6">
+              All explorations were created in Figma with animations developed
+              in <span className="font-medium text-[#254fdb]">Jitter</span>. I
+              primarily used the{" "}
+              <span className="font-medium text-[#254fdb]">'Sora'</span>{" "}
+              typeface for its bold presence that commands attention and
+              readability from a distance. For the mock Vwaza Events website UI
+              and billboard poster, I incorporated{" "}
+              <span className="font-medium text-[#254fdb]">
+                Instrument Serif
+              </span>
+              , while{" "}
+              <span className="font-medium text-[#254fdb]">Albert Sans</span>
+              was selected for UI playlist cards (I noticed you're using{" "}
+              <span className="font-medium text-[#254fdb]">Proxima Nova</span>
+              on your website, but chose freely available alternatives for this
+              showcase).
+            </p>
+            <p className="text-[#d5d5d5] text-[15px]">
+              I've maintained alignment with your brand colors—sky blue and blue
+              on critical visuals, especially the logo treatments. All images
+              are sourced from Lummipics (
+              <span className="font-medium text-[#254fdb]">lummi.ai</span>)
+              unless specified otherwise, with mockups created using Figma
+              plugins.
+            </p>
+            <p className="text-[#d5d5d5] text-[15px]">
+              I believe joining Vwaza Multimedia would help me grow as a graphic
+              designer while pursuing my aspirations in{" "}
+              <span className="font-medium text-[#254fdb]">
+                motion graphics, illustration, and 3D design
+              </span>
+              . I'm excited about the possibility of contributing to your team
+              while developing my social and management skills.
+            </p>
+            <p className="text-[#d5d5d5] text-[15px] mb-14">
+              For additional examples of my work demonstrating my understanding
+              of form, typography, and color principles, please visit my Twitter
+              profile{" "}
+              <a
+                href="https://x.com/mwala_joel"
+                className="font-semibold underline text-[#25b1db]"
+              >
+                @mwala_joel
+              </a>
+              , where you'll find web design projects that showcase these
+              fundamental design capabilities.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
